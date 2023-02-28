fc4712efc670570f2e6f98e434001aa591cd1c51
FanFanicon <2628838256@qq.com>
2023-02-28 15:18:57 +0800

    简单修改一下

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use std::ffi::c_long;
+
+use actix_web::{middleware, web, App, HttpRequest, HttpServer,get, http::{
+        header::{self, ContentType},
+        Method, StatusCode,
+    },HttpResponse,Result};
+use actix_session::{storage::CookieSessionStore, Session, SessionMiddleware};
+use actix_files;
+
+
+
+async fn react_index() -> HttpResponse{
+    println!("docs");
+   HttpResponse::Ok().content_type("application/json").body("我尼玛docs")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            // enable logger
+            .wrap(middleware::Logger::default())
+            .service(actix_files::Files::new("/","./dist").index_file("index.html")) //处理静态文件
+            .default_service(
+                web::get().to(react_index)
+            )
+            
+            
+            
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
